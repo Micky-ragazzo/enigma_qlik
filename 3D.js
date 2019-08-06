0e94bca663c78fe0cddd0b6597385d9616001595
Micky-ragazzo <Micky-ragazzo@users.noreply.github.com>
2019-08-06 16:03:25 +0200

    Initial commit

--- 3D.js
+++ 3D.js
@@ -1,84 +1,20 @@
-define(['./properties', 'text!./template.html'],
-    function(properties, template) {
+define(["qlik", "text!./template.html", "./node_modules/jquery/dist/jquery.js", "css!./3D.css"],
+    function(qlik, template) {
+
         return {
-            definition: properties,
-            paint: function($element) {
-                //add your rendering code here
-                $element.html("3D");
-                //needed for export
-                return qlik.Promise.resolve();
-            },
-            resize: function() {},
             template: template,
-            definition: properties,
-            dimportProperties: function() {},
-            dshowRequirements: function() {},
-            updateData: function() {}, //Feb 18'
-            support: {
-                snapshot: true,
-                export: true,
-                exportData: true
-            },
-            getExportRawDataOptions: function() {},
-            controller: [],
-            initialProperties: {
-                version: 1.0,
-                qHyperCubeDef: {
-                    qDimensions: [],
-                    qMeasures: [],
-                    qInitialDataFetch: [{
-                        qWidth: 2,
-                        qHeight: 50
-                    }]
-                }
-            },
-            controller: ['$scope', '$element', function(scope, $element) {
-                $element.html("3D");
-            }]
-        };
-    });
-
-/*define(["qlik"],
-    function(qlik) {
-
-        return {
             support: {
                 snapshot: true,
                 export: true,
                 exportData: false
             },
-            paint: function($element) {
-                //add your rendering code here
-                $element.html("3D");
-                //needed for export
+            paint: function() {
                 return qlik.Promise.resolve();
             },
-            resize: function($element, layout) {
-                this.paint($element, layout);
-            },
-            initialProperties: {
-                qHyperCubeDef: {
-                    qDimensions: [], //the 
-                    qMeasures: [],
-                    qInitialDataFetch: [{
-                        qWidth: 2,
-                        qHeight: 50
-                    }]
-                }
-            },
-            updateData: function() {}, //Feb 18'
-            //definition: properties,
-            dimportProperties: function() {},
-            dshowRequirements: function() {},
-            support: {
-                snapshot: true,
-                export: true,
-                exportData: true
-            },
-            getExportRawDataOptions: function() {},
-            controller: []
-                //template: HTMLtemplate,
+            controller: ['$scope', function($scope) {
+                //add your rendering code here
+                $scope.name = "MA";
+            }]
         };
 
-	});
-*/
+    });

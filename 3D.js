00fea35bfe4eb3b6dbed6aee654adedce198c546
Micky-ragazzo <Micky-ragazzo@users.noreply.github.com>
2019-08-14 15:13:55 +0200

    Your commit message

--- 3D.js
+++ 3D.js
@@ -1,18 +1,35 @@
-define([
-        './properties',
-        './initialproperties',
-        'text!./template.html'
-    ],
-    function(props, initProps, ngTemplate) {
-        'use strict';
+define(["./properties", "qlik", "text!./template.html"],
+    function(properties, qlik, template) {
 
         return {
-            definition: props,
-            initialProperties: initProps,
-            snapshot: { canTakeSnapshot: true },
-            template: ngTemplate,
-            controller: ['$scope', function($scope) {
-                $scope.myTitle = 'This is my AngularJS table';
+            definition: properties,
+            template: template,
+            initialProperties: {
+                version: 1.0,
+                qHyperCubeDef: { //hypercube can contain both dimensions and measures. If selections are applied to a hypercube, only the selected values are displayed.
+                    qDimensions: [],
+                    qMeasures: [],
+                    qInterColumnSortOrder: [1, 0],
+                    qInitialDataFetch: [{ //set at  point as it defines the size of the underlying data that the extension retrieves from the Qlik Engine.
+                        qWidth: 2,
+                        qHeight: 50
+                    }]
+                }
+            },
+            support: {
+                snapshot: true,
+                export: true,
+                exportData: false
+            },
+            paint: function() {
+                return qlik.Promise.resolve();
+            },
+            controller: ['$scope', function($scope) { //controller and template are the standard AngularJS components required to render an extension using AngularJS.
+                //add your rendering code here
+                $scope.firstName = "John";
+                $scope.lastName = "Doe";
+                console.log($scope.html);
             }]
         };
+
     });
--- properties.js
+++ properties.js
@@ -1,19 +1,66 @@
-define([], function() {
-    'use strict';
-
-    var dimensions = { uses: "dimensions" };
-    var measures = { uses: "measures" };
-    var sorting = { uses: "sorting" };
-    var addons = { uses: "addons" };
-    var appearancePanel = { uses: "settings" };
-    return {
-        type: "items",
-        component: "accordion",
-        items: {
-            dimensions: dimensions,
-            measures: measures,
-            sorting: sorting,
-            appearance: appearancePanel
-        }
-    };
-});
+define([],
+        function() {
+            return {
+                type: "items",
+                component: "accordion",
+                items: {
+                    dimensions: {
+                        uses: "dimensions",
+                        min: 1,
+                        max: 1
+                    },
+                    measures: {
+                        uses: "measures",
+                        min: 1,
+                        max: 1
+                    },
+                    sorting: {
+                        uses: "sorting"
+                    },
+                    addons: {
+                        uses: "addons"
+                    },
+                    settings: {
+                        uses: "settings",
+                        items: {
+                            barColor: {
+                                label: "Bar Color",
+                                component: "color-picker",
+                                type: "object",
+                                dualOutput: true,
+                                schemaIgnore: true,
+                                ref: "barColor",
+                                defaultValue: {
+                                    index: -1,
+                                    color: "#657dbc"
+                                },
+                                show: function(layout) {
+                                    return true
+                                },
+                                horizontalReference: {
+                                    type: "array",
+                                    ref: "listItems",
+                                    label: "Horizontal Reference Line",
+                                    itemTitleRef: "reflabel",
+                                    allowAdd: true,
+                                    allowRemove: true,
+                                    addTranslation: "Add Reference Line",
+                                    items: {
+                                        label: {
+                                            type: "string",
+                                            ref: "reflabel",
+                                            label: "Reference Line Label",
+                                            expression: "optional"
+                                        },
+                                        refvalue: {
+                                            label: "Reference Value",
+                                            type: "number",
+                                            ref: "refvalue"
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                };
+            });
